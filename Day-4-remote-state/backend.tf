e8823df7f46ccac08b9de4dc95af5415f0a1c53f
sree <sreemukanya05@gmail.com>
2026-01-03 20:53:43 +0530

    lockfile

--- Day-4-remote-state/backend.tf
+++ Day-4-remote-state/backend.tf
@@ -2,6 +2,8 @@ terraform {
   backend "s3" {
     bucket = "saidharsha123"
     key    = "terraform.tfstate"
+    #Enable s3 native locking
+    use_lockfile = true
     region = "us-east-1"
   }
 }
